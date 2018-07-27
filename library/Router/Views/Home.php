a8eeaafe31bae9b47c92e7137c1e533faa2d1705
Zeryther <zeryther@gmail.com>
2018-07-27 19:44:35 +0000

    Added login form to home page

--- library/Router/Views/Home.php
+++ library/Router/Views/Home.php
@@ -1,3 +1,25 @@
 <div class="card-body">
-	home
+	<div class="row">
+		<div class="col-lg-4">
+			<div class="card">
+				<div class="card-body">
+					<form action="<?= $app->routeUrl("/login"); ?>" method="post">
+						<h4 class="text-center">Log In</h4>
+						<?= Util::insertCSRFToken(); ?>
+						<input type="text" name="email" placeholder="E-Mail Address or Username" class="form-control mb-2"/>
+						<input type="password" name="password" placeholder="Password" class="form-control mb-2"/>
+						<input type="submit" class="btn btn-primary btn-block mb-1" value="Sign In"/>
+						
+						<a href="<?= $app->routeUrl("/register"); ?>" class="btn btn-light btn-block my-0">Create an account</a>
+
+						<a href="<?= $app->routeUrl("/gigadriveLogin"); ?>" class="btn btn-success btn-block mt-1">Login with Gigadrive</a>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-8">
+			right
+		</div>
+	</div>
 </div>
