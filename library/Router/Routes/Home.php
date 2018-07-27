2ca8c80e38cda4eedfe2371c2a24a6cdc5d115c4
Zeryther <zeryther@gmail.com>
2018-07-27 21:49:23 +0000

    Added nav variable on home page

--- library/Router/Routes/Home.php
+++ library/Router/Routes/Home.php
@@ -3,13 +3,15 @@
 $app->bind("/",function(){
 	if(!Util::isLoggedIn()){
 		$data = array(
-			"title" => "Home"
+			"title" => "Home",
+			"nav" => NAV_HOME
 		);
 	
 		return $this->render("views:Home.php with views:Layout.php",$data);
 	} else {
 		$data = array(
-			"title" => "Home"
+			"title" => "Home",
+			"nav" => NAV_HOME
 		);
 	
 		return $this->render("views:HomeFeed.php with views:Layout.php",$data);
