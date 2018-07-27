df60145f83573881c5e51dae95eaf467eccd993b
Zeryther <zeryther@gmail.com>
2018-07-27 22:11:07 +0000

    Added layout to profile page

--- library/Router/Views/Profile.php
+++ library/Router/Views/Profile.php
@@ -1,1 +1,12 @@
-<?php var_dump($user);
+<div class="card-body">
+	<div class="row">
+		<div class="col-lg-3">
+			<h4 class="mb-0"><?= $user->getUsername(); ?></h4>
+			<p class="text-muted my-0" style="font-size: 16px">@<?= $user->getUsername(); ?></p>
+		</div>
+
+		<div class="col-lg-9">
+			right
+		</div>
+	</div>
+</div>
