2bdb911afbe5bf4a10f76ce1e26c034487a4e654
Zeryther <zeryther@gmail.com>
2018-07-27 19:46:47 +0000

    Updated 404 page

--- library/Router/Views/ErrorPages/404.php
+++ library/Router/Views/ErrorPages/404.php
@@ -1,5 +1,7 @@
-<h1>Error 404: Page not found</h1>
+<div class="card-body">
+	<h1>Error 404: Page not found</h1>
 
-<p class="mb-0">
-	The requested page or file could not be found.
-</p>
+	<p class="mb-0">
+		The requested page or file could not be found.
+	</p>
+</div>
