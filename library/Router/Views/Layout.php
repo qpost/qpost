2e22f37ebb279fa9adf47dec69be2fa2476d1358
Zeryther <zeryther@gmail.com>
2018-07-28 18:01:27 +0000

    Added more meta shit

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -6,10 +6,43 @@ if(isset($title) && !empty($title)){
 	$title = $app["config.site"]["name"];
 }
 
+if(!isset($description) || empty($description))
+	$description = DEFAULT_DESCRIPTION;
+
+if(!isset($socialImage) || empty($socialImage))
+	$socialImage = DEFAULT_TWITTER_IMAGE;
+
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title><?= $title ?></title>
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+		<meta http-equiv="x-ua-compatible" content="ie=edge"/>
+		<meta name="apple-mobile-web-app-capable" content="yes">
+
+		<meta name="og:site_name" content="<?= $app["config.site"]["name"] ?>" />
+		<meta name="og:title" content='<?= $title; ?>' />
+		<meta name="og:description" content="<?= $description; ?>" />
+		<meta name="og:locale" content="<?= LANGUAGE; ?>" />
+
+		<meta name="twitter:title" content="<?= $title; ?>" />
+		<meta name="twitter:description" content="<?= $description; ?>" />
+		<meta name="twitter:image" content="<?= $socialImage; ?>" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@mcskinhistory" />
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#007BFF">
+		<meta name="theme-color" content="#007BFF">
+
+		<meta name="description" content="<?= $description; ?>" />
+		<meta name="keywords" content="social,network,posts,profiles,pictures,bio,gigadrive" />
 
 		<noscript><meta http-equiv="refresh" content="0; URL=https://gigadrivegroup.com/badbrowser"></noscript>
 
