c21cf370cbd9379b3590ff0cf0a60740c51a8c2f
Zeryther <zeryther@gmail.com>
2018-07-28 15:10:14 +0000

    Added followers and following profile routes

--- library/Router/Routes/Profile.php
+++ library/Router/Routes/Profile.php
@@ -1,5 +1,117 @@
 <?php
 
+$app->bind("/:query/following",function($params){
+	$query = $params["query"];
+	$page = 1;
+
+	if(!empty($query)){
+		$user = User::getUserByUsername($query);
+		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
+
+		if(!is_null($user)){
+			if($query !== $user->getUsername())
+				$this->reroute("/" . $user->getUsername());
+
+			$data = array(
+				"title" => $user->getUsername(),
+				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+				"user" => $user,
+				"socialImage" => $user->getAvatarURL(),
+				"showProfile" => true,
+				"profileTab" => PROFILE_TAB_POSTS,
+				"currentPage" => $page,
+				"subtitle" => "People followed by " . $user->getUsername()
+			);
+		
+			return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+		}
+	}
+});
+
+$app->bind("/:query/following/:page",function($params){
+	$query = $params["query"];
+	$page = is_numeric($params["page"]) && (int)$params["page"] > 0 ? (int)$params["page"] : 1;
+
+	if(!empty($query)){
+		$user = User::getUserByUsername($query);
+		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
+
+		if(!is_null($user)){
+			if($query !== $user->getUsername())
+				$this->reroute("/" . $user->getUsername());
+
+			$data = array(
+				"title" => $user->getUsername(),
+				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+				"user" => $user,
+				"socialImage" => $user->getAvatarURL(),
+				"showProfile" => true,
+				"profileTab" => PROFILE_TAB_POSTS,
+				"currentPage" => $page,
+				"subtitle" => "People followed by " . $user->getUsername()
+			);
+		
+			return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+		}
+	}
+});
+
+$app->bind("/:query/followers",function($params){
+	$query = $params["query"];
+	$page = 1;
+
+	if(!empty($query)){
+		$user = User::getUserByUsername($query);
+		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
+
+		if(!is_null($user)){
+			if($query !== $user->getUsername())
+				$this->reroute("/" . $user->getUsername());
+
+			$data = array(
+				"title" => $user->getUsername(),
+				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+				"user" => $user,
+				"socialImage" => $user->getAvatarURL(),
+				"showProfile" => true,
+				"profileTab" => PROFILE_TAB_POSTS,
+				"currentPage" => $page,
+				"subtitle" => "People following " . $user->getUsername()
+			);
+		
+			return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+		}
+	}
+});
+
+$app->bind("/:query/followers/:page",function($params){
+	$query = $params["query"];
+	$page = is_numeric($params["page"]) && (int)$params["page"] > 0 ? (int)$params["page"] : 1;
+
+	if(!empty($query)){
+		$user = User::getUserByUsername($query);
+		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
+
+		if(!is_null($user)){
+			if($query !== $user->getUsername())
+				$this->reroute("/" . $user->getUsername());
+
+			$data = array(
+				"title" => $user->getUsername(),
+				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+				"user" => $user,
+				"socialImage" => $user->getAvatarURL(),
+				"showProfile" => true,
+				"profileTab" => PROFILE_TAB_POSTS,
+				"currentPage" => $page,
+				"subtitle" => "People following " . $user->getUsername()
+			);
+		
+			return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+		}
+	}
+});
+
 $app->bind("/:query",function($params){
 	$query = $params["query"];
 	$page = 1;
