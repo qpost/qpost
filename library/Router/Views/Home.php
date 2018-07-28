cf655dc9160ab9fc6730a160d86fe20260cb0e99
Zeryther <zeryther@gmail.com>
2018-07-28 18:10:28 +0000

    Added text to home page

--- library/Router/Views/Home.php
+++ library/Router/Views/Home.php
@@ -10,7 +10,11 @@
 		</div>
 
 		<div class="col-lg-8">
-			right
+			<p class="mb-0">
+				<b>Welcome to <?= $app["config.site"]["name"] ?>!</b>
+				<br/><br/>
+				<?= $app["config.site"]["name"] ?> is a social network that uses the Gigadrive account system. To sign in, please create an account on <b><a href="https://gigadrivegroup.com" target="_blank">gigadrivegroup.com</a></b> and click the green button on the left on this page to connect it.
+			</p>
 		</div>
 	</div>
 </div>
