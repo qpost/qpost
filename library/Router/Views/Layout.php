dad8a6d2e95903a0489d86cf3906fe24b648257e
Zeryther <zeryther@gmail.com>
2018-07-28 17:58:30 +0000

    Added noscript redirect

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -11,6 +11,8 @@ if(isset($title) && !empty($title)){
 	<head>
 		<title><?= $title ?></title>
 
+		<noscript><meta http-equiv="refresh" content="0; URL=https://gigadrivegroup.com/badbrowser"></noscript>
+
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
 		<?= $app->style([
