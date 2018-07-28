efceaa2882bc8dd464cad02ce23039ac6100ad77
Zeryther <zeryther@gmail.com>
2018-07-28 19:38:23 +0000

    Added search form to home feed

--- library/Router/Views/HomeFeed.php
+++ library/Router/Views/HomeFeed.php
@@ -1,3 +1,19 @@
-<?php
+<div class="card-body">
+	<div class="row">
+		<div class="col-lg-8">
+			<h4>Feed</h4>
+		</div>
 
-// TODO
+		<div class="col-lg-4">
+			<form action="<?= $app->routeUrl("/search"); ?>" method="get">
+				<div class="input-group input-group-sm">
+					<input class="form-control" name="query" placeholder="Search <?= $app["config.site"]["name"] ?>" type="text"/>
+
+					<div class="input-group-append">
+						<button class="btn btn-primary px-3" type="submit"><i class="fas fa-search"></i></button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
