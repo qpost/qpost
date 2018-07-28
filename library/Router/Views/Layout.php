c6f8a9b177899b126dbd58590e1b8a8c47558735
Zeryther <zeryther@gmail.com>
2018-07-28 17:58:18 +0000

    Added font awesome

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -11,6 +11,8 @@ if(isset($title) && !empty($title)){
 	<head>
 		<title><?= $title ?></title>
 
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
 		<?= $app->style([
 			"assets:css/bootstrap.min.css",
 			"https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css",
