34715dde302e4cb270a0417e5e924278e01768ec
Zeryther <zeryther@gmail.com>
2018-07-29 13:41:37 +0000

    Added privacy level closed profile view

--- library/Router/Routes/Profile.php
+++ library/Router/Routes/Profile.php
@@ -9,21 +9,27 @@ $app->bind("/:query/following",function($params){
 		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
 		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+				return $this->reroute("/" . $user->getUsername());
+			} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+				return $this->reroute("/" . $user->getUsername());
+			} else {
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => "People followed by " . $user->getUsername(),
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FOLLOWING,
-				"currentPage" => $page,
-				"subtitle" => "People followed by " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+				$data = array(
+					"title" => "People followed by " . $user->getUsername(),
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FOLLOWING,
+					"currentPage" => $page,
+					"subtitle" => "People followed by " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
@@ -37,21 +43,27 @@ $app->bind("/:query/following/:page",function($params){
 		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
 		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+				return $this->reroute("/" . $user->getUsername());
+			} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+				return $this->reroute("/" . $user->getUsername());
+			} else {
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => "People followed by " . $user->getUsername(),
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FOLLOWING,
-				"currentPage" => $page,
-				"subtitle" => "People followed by " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+				$data = array(
+					"title" => "People followed by " . $user->getUsername(),
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FOLLOWING,
+					"currentPage" => $page,
+					"subtitle" => "People followed by " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Following.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
@@ -65,21 +77,27 @@ $app->bind("/:query/followers",function($params){
 		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
 		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+				return $this->reroute("/" . $user->getUsername());
+			} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+				return $this->reroute("/" . $user->getUsername());
+			} else {
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => "People following " . $user->getUsername(),
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FOLLOWERS,
-				"currentPage" => $page,
-				"subtitle" => "People following " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+				$data = array(
+					"title" => "People following " . $user->getUsername(),
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FOLLOWERS,
+					"currentPage" => $page,
+					"subtitle" => "People following " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
@@ -93,21 +111,27 @@ $app->bind("/:query/followers/:page",function($params){
 		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
 		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+				return $this->reroute("/" . $user->getUsername());
+			} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+				return $this->reroute("/" . $user->getUsername());
+			} else {
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => "People following " . $user->getUsername(),
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FOLLOWERS,
-				"currentPage" => $page,
-				"subtitle" => "People following " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+				$data = array(
+					"title" => "People following " . $user->getUsername(),
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FOLLOWERS,
+					"currentPage" => $page,
+					"subtitle" => "People following " . $user->getUsername() . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? '<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Followers.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
@@ -121,21 +145,37 @@ $app->bind("/:query",function($params){
 		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
 		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+				$data = array(
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL()
+				);
+			
+				return $this->render("views:Profile/ClosedLevel.php with views:Layout.php",$data);
+			} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+				$data = array(
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL()
+				);
+			
+				return $this->render("views:Profile/PrivateLevel.php with views:Layout.php",$data);
+			} else {
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => $user->getDisplayName() . " (@" . $user->getUsername() . ")",
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FEED,
-				"currentPage" => $page,
-				"subtitle" => $user->getUsername() . "'s profile" . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? ' (This is you!)<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Feed.php with views:Layout.php",$data);
+				$data = array(
+					"title" => $user->getDisplayName() . " (@" . $user->getUsername() . ")",
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FEED,
+					"currentPage" => $page,
+					"subtitle" => $user->getUsername() . "'s profile" . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? ' (This is you!)<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Feed.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
@@ -145,25 +185,31 @@ $app->bind("/:query/:page",function($params){
 	$page = is_numeric($params["page"]) && (int)$params["page"] > 0 ? (int)$params["page"] : 1;
 
 	if(!empty($query)){
-		$user = User::getUserByUsername($query);
-		if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
+		if($user->getPrivacyLevel() == PRIVACY_LEVEL_CLOSED && $user->getId() != $_SESSION["id"]){
+			return $this->reroute("/" . $user->getUsername());
+		} else if($user->getPrivacyLevel() == PRIVACY_LEVEL_PRIVATE && (!Util::isLoggedIn() || ($user->getId() != $_SESSION["id"] && !$user->isFollower($_SESSION["id"])))){
+			return $this->reroute("/" . $user->getUsername());
+		} else {
+			$user = User::getUserByUsername($query);
+			if(is_null($user) && is_numeric($query)) $user = @User::getUserById((int)$query);
 
-		if(!is_null($user)){
-			if($query !== $user->getUsername())
-				$this->reroute("/" . $user->getUsername());
+			if(!is_null($user)){
+				if($query !== $user->getUsername())
+					$this->reroute("/" . $user->getUsername());
 
-			$data = array(
-				"title" => $user->getDisplayName() . " (@" . $user->getUsername() . ")",
-				"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
-				"user" => $user,
-				"socialImage" => $user->getAvatarURL(),
-				"showProfile" => true,
-				"profileTab" => PROFILE_TAB_FEED,
-				"currentPage" => $page,
-				"subtitle" => $user->getUsername() . "'s profile" . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? ' (This is you!)<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
-			);
-		
-			return $this->render("views:Profile/Feed.php with views:Layout.php",$data);
+				$data = array(
+					"title" => $user->getDisplayName() . " (@" . $user->getUsername() . ")",
+					"nav" => Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? NAV_PROFILE : null,
+					"user" => $user,
+					"socialImage" => $user->getAvatarURL(),
+					"showProfile" => true,
+					"profileTab" => PROFILE_TAB_FEED,
+					"currentPage" => $page,
+					"subtitle" => $user->getUsername() . "'s profile" . (Util::isLoggedIn() && $user->getId() == $_SESSION["id"] ? ' (This is you!)<a class="float-right text-white" href="/edit">Edit profile</a>' : "")
+				);
+			
+				return $this->render("views:Profile/Feed.php with views:Layout.php",$data);
+			}
 		}
 	}
 });
