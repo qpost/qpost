ccc5c2c089a67b55cb3ad761d3cb25134f3db882
Mehdi Baaboura <zeryther@gmail.com>
2018-08-02 18:42:12 +0200

    Vertically center block dialog

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -300,7 +300,7 @@ if(!isset($socialImage) || empty($socialImage))
 							</button>
 
 							<div class="modal fade" id="blockModal" tabindex="-1" role="dialog" aria-labelledby="blockModalLabel" aria-hidden="true">
-								<div class="modal-dialog" role="document">
+								<div class="modal-dialog modal-dialog-centered" role="document">
 									<div class="modal-content">
 										<div class="modal-header">
 											<h5 class="modal-title" id="blockModalLabel">Block @<?= $user->getUsername(); ?></h5>
