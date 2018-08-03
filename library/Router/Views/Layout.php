1e52fabd8e5dc3a0acde8ca0d1667a6611c08985
Mehdi Baaboura <zeryther@gmail.com>
2018-08-03 02:51:57 +0200

    Added status modal dummy

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -236,6 +236,18 @@ if(!isset($socialImage) || empty($socialImage))
 					</div>
 				</nav>
 
+				<div class="modal fade" id="statusModal" tabindex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-body">
+								<div class="text-center">
+									<i class="fas fa-spinner fa-pulse"></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
 				<?php
 
 				if(isset($subtitle)){
