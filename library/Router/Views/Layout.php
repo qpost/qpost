57a8e7cff3e0f97d6edaed345b7e2fe08bb109c6
Zeryther <zeryther@gmail.com>
2018-08-04 03:59:45 +0300

    Updated meta and icons

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -30,15 +30,16 @@ if(!isset($socialImage) || empty($socialImage))
 		<meta name="twitter:description" content="<?= Util::sanatizeHTMLAttribute($description) ?>" />
 		<meta name="twitter:image" content="<?= Util::sanatizeHTMLAttribute($socialImage) ?>" />
 		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:site" content="@mcskinhistory" />
 
 		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 		<link rel="manifest" href="/site.webmanifest">
-		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-		<meta name="msapplication-TileColor" content="#007BFF">
-		<meta name="theme-color" content="#007BFF">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#007bff">
+		<meta name="apple-mobile-web-app-title" content="qpost">
+		<meta name="application-name" content="qpost">
+		<meta name="msapplication-TileColor" content="#007bff">
+		<meta name="theme-color" content="#007bff">
 
 		<meta name="description" content="<?= Util::sanatizeHTMLAttribute($description) ?>" />
 		<meta name="keywords" content="social,network,posts,profiles,pictures,bio,gigadrive" />
