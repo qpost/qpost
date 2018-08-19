fcd09f37a8333d7c95aee0a463911b4e18fa9d67
Zeryther <zeryther@gmail.com>
2018-08-19 14:02:16 +0200

    Added register form to home page

--- library/Router/Views/Home.php
+++ library/Router/Views/Home.php
@@ -11,18 +11,34 @@
 						Follow the people you are interested in.
 					</p>
 
-					<p class="text-muted mt-3">
-						Sign in with Gigadrive to get started.
-					</p>
+					<a href="/login/gigadrive" class="btn btn-success clearUnderline btn-block" data-no-instant>Sign in with Gigadrive</a>
 
-					<a href="/login" class="btn btn-success clearUnderline btn-block" data-no-instant>Sign in with Gigadrive</a>
-					<a href="https://gigadrivegroup.com/register" target="_blank" class="btn btn-light clearUnderline btn-block">Create a Gigadrive account</a>
+					<div class="text-center text-muted font-weight-bold small my-2">OR</div>
+
+					<form action="/login" method="post">
+						<?= Util::insertCSRFToken() ?>
+						<input type="email" class="form-control mb-2" name="email" placeholder="Email"/>
+						<input type="text" class="form-control mb-2" name="displayName" placeholder="Full Name"/>
+						<input type="text" class="form-control mb-2" name="username" placeholder="Username"/>
+						<input type="password" class="form-control mb-2" name="password" placeholder="Password"/>
+						<input type="submit" class="btn btn-primary btn-block" value="Register"/>
+					</form>
+
+					<div class="mt-2 small">
+						By clicking Register you agree to our <a href="https://gigadrivegroup.com/legal/terms-of-service" target="_blank">Terms of Service</a> and <a href="https://gigadrivegroup.com/legal/privacy-policy" target="_blank">Privacy Policy</a>.
+					</div>
+				</div>
+			</div>
+
+			<div class="card mt-3">
+				<div class="card-body text-center">
+					Already have an account? <a href="/login">Log in</a>
 				</div>
 			</div>
 		</div>
 
 		<div class="col-lg-8">
-			<img src="/assets/img/responsivedemo.png" style="width: 100%"/>
+			<img src="/assets/img/responsivedemo.png" class="mt-5" style="width: 100%"/>
 		</div>
 	</div>
 </div>
