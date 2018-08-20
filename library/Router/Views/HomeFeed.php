6567c9a5ad7ddf0d982ec4bbdf8de053695b103d
Zeryther <zeryther@gmail.com>
2018-08-20 14:43:17 +0200

    Fixed user box limit

--- library/Router/Views/HomeFeed.php
+++ library/Router/Views/HomeFeed.php
@@ -144,10 +144,12 @@
 					$trendingUsers = [];
 					$n = "trendingUsers";
 
+					$limit = 5;
+
 					if(CacheHandler::existsInCache($n)){
 						$trendingUsers = CacheHandler::getFromCache($n);
 					} else {
-						$stmt = $mysqli->prepare("SELECT COUNT(f.following) as `increase`,u.* FROM `users` AS u LEFT JOIN `follows` AS f ON f.`following` = u.`id` WHERE f.`time` > (NOW() - INTERVAL 24 HOUR) AND u.`privacy.level` = 'PUBLIC' GROUP BY u.`id` ORDER BY `increase` DESC LIMIT 5");
+						$stmt = $mysqli->prepare("SELECT COUNT(f.following) as `increase`,u.* FROM `users` AS u LEFT JOIN `follows` AS f ON f.`following` = u.`id` WHERE f.`time` > (NOW() - INTERVAL 24 HOUR) AND u.`privacy.level` = 'PUBLIC' GROUP BY u.`id` ORDER BY `increase` DESC LIMIT " . $limit);
 						if($stmt->execute()){
 							$result = $stmt->get_result();
 							
@@ -171,7 +173,7 @@
 					if(CacheHandler::existsInCache($n)){
 						$newUsers = CacheHandler::getFromCache($n);
 					} else {
-						$stmt = $mysqli->prepare("SELECT * FROM `users` WHERE `privacy.level` = 'PUBLIC' ORDER BY `time` DESC LIMIT 5");
+						$stmt = $mysqli->prepare("SELECT * FROM `users` WHERE `privacy.level` = 'PUBLIC' ORDER BY `time` DESC LIMIT " . $limit);
 						if($stmt->execute()){
 							$result = $stmt->get_result();
 							
@@ -190,7 +192,7 @@
 
 					// query is a combination of https://stackoverflow.com/a/12915720 and https://stackoverflow.com/a/24165699
 					$suggestedUsers = [];
-					$stmt = $mysqli->prepare("SELECT COUNT(*)       AS mutuals, u.* FROM users      AS me INNER JOIN follows    AS my_friends ON my_friends.follower = me.id INNER JOIN follows    AS their_friends ON their_friends.follower = my_friends.following INNER JOIN  users 	   AS u ON u.id = their_friends.following WHERE me.id = ? AND their_friends.following != ? AND NOT EXISTS (SELECT 1 FROM follows fu3 WHERE fu3.follower = ? AND fu3.following = their_friends.following) GROUP BY me.id, their_friends.following");
+					$stmt = $mysqli->prepare("SELECT COUNT(*)       AS mutuals, u.* FROM users      AS me INNER JOIN follows    AS my_friends ON my_friends.follower = me.id INNER JOIN follows    AS their_friends ON their_friends.follower = my_friends.following INNER JOIN  users 	   AS u ON u.id = their_friends.following WHERE me.id = ? AND their_friends.following != ? AND NOT EXISTS (SELECT 1 FROM follows fu3 WHERE fu3.follower = ? AND fu3.following = their_friends.following) GROUP BY me.id, their_friends.following LIMIT " . $limit);
 					$stmt->bind_param("iii",$currentUser,$currentUser,$currentUser);
 					if($stmt->execute()){
 						$result = $stmt->get_result();
