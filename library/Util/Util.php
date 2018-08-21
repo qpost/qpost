598b1de65fe7ad2ebf08b7d34aba42b321bc7389
Zeryther <zeryther@gmail.com>
2018-08-21 11:48:20 +0200

    Fix

--- library/Util/Util.php
+++ library/Util/Util.php
@@ -1171,14 +1171,16 @@ class Util {
 		$postActionButtons = "";
 
 		if(Util::isLoggedIn()){
+			$currentUser = Util::getCurrentUser();
+
 			$postActionButtons .= '<div class="mt-1 postActionButtons ignoreParentClick float-left">';
 			$postActionButtons .= '<span class="replyButton" data-toggle="tooltip" title="Reply" data-reply-id="' . $post->getId() . '">';
 			$postActionButtons .= '<i class="fas fa-share"></i>';
 			$postActionButtons .= '</span><span class="replyCount small text-primary mx-2">';
 			$postActionButtons .= $post->getReplies();
 			$postActionButtons .= '</span>';
-			$postActionButtons .= '<span' . (Util::getCurrentUser()->getId() != $post->getUser()->getId() ? ' class="shareButton" data-toggle="tooltip" title="Share"' : ' data-toggle="tooltip" title="You can not share this post"') . ' data-post-id="' . $post->getId() . '">';
-			$postActionButtons .= '<i class="fas fa-share-alt' . (Util::getCurrentUser()->hasShared($post->getId()) ? ' text-primary' : "")  . '"' . (Util::getCurrentUser()->hasShared($post->getId()) ? "" : ' style="color: gray"') . '></i>';
+			$postActionButtons .= '<span' . ($currentUser->getId() != $post->getUser()->getId() ? ' class="shareButton" data-toggle="tooltip" title="Share"' : ' data-toggle="tooltip" title="You can not share this post"') . ' data-post-id="' . $post->getId() . '">';
+			$postActionButtons .= '<i class="fas fa-share-alt' . ($currentUser->hasShared($post->getId()) ? ' text-primary' : "")  . '"' . ($currentUser->hasShared($post->getId()) ? "" : ' style="color: gray"') . '></i>';
 			$postActionButtons .= '</span>';
 
 			$postActionButtons .= '<span class="shareCount small text-primary ml-2 mr-2">';
@@ -1186,14 +1188,14 @@ class Util {
 			$postActionButtons .= '</span>';
 
 			$postActionButtons .= '<span class="favoriteButton" data-post-id="' . $post->getId() . '">';
-			$postActionButtons .= '<i class="fas fa-star"' . (Util::getCurrentUser()->hasFavorited($post->getId()) ? ' style="color: gold"' : ' style="color: gray"') . '></i>';
+			$postActionButtons .= '<i class="fas fa-star"' . ($currentUser->hasFavorited($post->getId()) ? ' style="color: gold"' : ' style="color: gray"') . '></i>';
 			$postActionButtons .= '</span>';
 
 			$postActionButtons .= '<span class="favoriteCount small ml-2 mr-4" style="color: #ff960c">';
 			$postActionButtons .= $post->getFavorites();
 			$postActionButtons .= '</span>';
 
-			if(Util::getCurrentUser()->getId() == $post->getUserId()){
+			if($currentUser->getId() == $post->getUserId()){
 				$postActionButtons .= '<span class="deleteButton ml-2" data-post-id="' . $post->getId() . '" data-toggle="tooltip" title="Delete">';
 				$postActionButtons .= '<i class="fas fa-trash-alt"></i>';
 				$postActionButtons .= '</span>';
