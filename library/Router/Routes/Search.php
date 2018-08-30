cd6aa9c0b688d64d650106af92f09df6466be733
Zeryther <zeryther@gmail.com>
2018-08-30 20:12:37 +0200

    Fixed search title

--- library/Router/Routes/Search.php
+++ library/Router/Routes/Search.php
@@ -2,7 +2,7 @@
 
 $app->get("/search",function(){
 	$data = array(
-		"title" => "Search" . (isset($_GET["query"]) && !empty(trim($_GET["query"])) ? ": &quot;" . Util::sanatizeString($_GET["query"]) . "&quot;" : ""),
+		"title" => "Search" . (isset($_GET["query"]) && !empty(trim($_GET["query"])) ? ": \"" . Util::sanatizeString($_GET["query"]) . "\"" : ""),
 		"page" => isset($_GET["page"]) && !empty(trim($_GET["page"])) && is_numeric($_GET["page"]) && (int)$_GET["page"] > 0 ? (int)$_GET["page"] : 1,
 		"type" => isset($_GET["type"]) && !empty(trim($_GET["type"])) ? $_GET["type"] : "posts",
 		"query" => isset($_GET["query"]) && !empty(trim($_GET["query"])) ? $_GET["query"] : null
