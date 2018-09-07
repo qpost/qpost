106883ff08f8295a2b4872294238fd32f7a35c9c
Zeryther <zeryther@gmail.com>
2018-09-07 19:59:22 +0200

    Fixed logout

--- library/Router/Routes/Logout.php
+++ library/Router/Routes/Logout.php
@@ -3,5 +3,14 @@
 $app->bind("/logout",function(){
 	unset($_SESSION["id"]);
 
+	if(Util::isLoggedIn()){
+		$token = Token::getTokenById($_COOKIE["sesstoken"]);
+	
+		if(!is_null($token)){
+			$token->expire();
+			Util::unsetCookie("sesstoken");
+		}
+	}
+
 	return $this->reroute("/");
 });
