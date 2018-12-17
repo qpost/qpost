2fb1f8339fb14c0d36664420708f42e5e740e8fc
Zeryther <zeryther@gmail.com>
2018-12-17 11:35:08 +0100

    Reduced media file cache time

--- src/Media/MediaFile.php
+++ src/Media/MediaFile.php
@@ -302,7 +302,7 @@ class MediaFile {
      * @access public
      */
     public function saveToCache(){
-        CacheHandler::setToCache("media_id_" . $this->id,$this,20*60);
-        CacheHandler::setToCache("media_sha256_" . $this->sha256,$this,20*60);
+        CacheHandler::setToCache("media_id_" . $this->id,$this,60);
+        CacheHandler::setToCache("media_sha256_" . $this->sha256,$this,60);
     }
 }
