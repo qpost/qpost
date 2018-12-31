6d2dd21b184e25eff2192167a8523cdb7ca01eb0
Zeryther <zeryther@gmail.com>
2018-12-31 16:48:16 +0100

    Fixed block modal

--- src/Router/Views/Layout.php
+++ src/Router/Views/Layout.php
@@ -232,6 +232,35 @@ $currentUser = Util::getCurrentUser();
 				<div class="modal fade" id="statusModal" tabindex="-1" role="dialog" aria-hidden="true"></div>
 				<div class="modal fade" id="mediaModal" tabindex="-1" role="dialog" aria-hidden="true"></div>
 				<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-hidden="true"></div>
+
+				<div class="modal fade" id="blockModal" aria-labelledby="blockModalLabel" aria-hidden="true" tabindex="-1" role="dialog">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="blockModalLabel">Block @<?= $user->getUsername() ?></h5>
+
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+
+							<div class="modal-body">
+								@<?= $user->getUsername(); ?> will no longer be able to follow or message you, and you will not see notifications from @<?= $user->getUsername(); ?>.
+							</div>
+
+							<div class="modal-footer">
+								<form action="/<?= $user->getUsername(); ?>" method="post">
+									<?= Util::insertCSRFToken(); ?>
+									<input type="hidden" name="action" value="block"/>
+									<button type="submit" class="btn btn-danger">Block</button>
+								</form>
+
+								<button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
 				<span class="dz-message"></span>
 				
 				<div class="preview-template d-none">
@@ -359,34 +388,6 @@ $currentUser = Util::getCurrentUser();
 											}
 										}
 												?>
-								</div>
-
-								<div class="modal fade" id="blockModal" tabindex="-1" role="dialog" aria-labelledby="blockModalLabel" aria-hidden="true">
-									<div class="modal-dialog modal-dialog-centered" role="document">
-										<div class="modal-content">
-											<div class="modal-header">
-												<h5 class="modal-title" id="blockModalLabel">Block @<?= $user->getUsername(); ?></h5>
-
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-													<span aria-hidden="true">&times;</span>
-												</button>
-											</div>
-
-											<div class="modal-body">
-												@<?= $user->getUsername(); ?> will no longer be able to follow or message you, and you will not see notifications from @<?= $user->getUsername(); ?>.
-											</div>
-
-											<div class="modal-footer">
-												<form action="/<?= $user->getUsername(); ?>" method="post">
-													<?= Util::insertCSRFToken(); ?>
-													<input type="hidden" name="action" value="block"/>
-													<button type="submit" class="btn btn-danger">Block</button>
-												</form>
-
-												<button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
-											</div>
-										</div>
-									</div>
 								</div>
 								<?php
 
