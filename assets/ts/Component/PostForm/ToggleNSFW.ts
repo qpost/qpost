aae2dfb0264a5976dca9b884999c4ad079a4fed2
Zeryther <zeryther@gmail.com>
2019-06-23 00:40:44 +0200

    Fixed NSFW toggle button not working

--- assets/ts/Component/PostForm/ToggleNSFW.ts
+++ assets/ts/Component/PostForm/ToggleNSFW.ts
@@ -6,13 +6,15 @@ export default class ToggleNSFW {
 		$(document).on("click", ".toggleNSFW", (e) => {
 			e.preventDefault();
 
-			if ($(this).hasClass("text-success")) {
+			const $this = $(e.currentTarget);
+
+			if ($this.hasClass("text-success")) {
 				// toggle on
-				$(this).removeClass("text-success").addClass("text-danger");
+				$this.removeClass("text-success").addClass("text-danger");
 				Util.updateTooltip(e.currentTarget, "NSFW: on");
 			} else {
 				// toggle off
-				$(this).removeClass("text-danger").addClass("text-success");
+				$this.removeClass("text-danger").addClass("text-success");
 				Util.updateTooltip(e.currentTarget, "NSFW: off");
 			}
 		});
