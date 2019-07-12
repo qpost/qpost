9d6a26a9281dbbfaad6bee6e7863b0a033d26d8a
Zeryther <zeryther@gmail.com>
2019-07-12 03:20:22 +0200

    Disable share buttons on own posts

--- assets/ts/Component/FeedEntry/Buttons/ShareButton.tsx
+++ assets/ts/Component/FeedEntry/Buttons/ShareButton.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import FeedEntry from "../../../Entity/Feed/FeedEntry";
 import {Col} from "reactstrap";
+import Auth from "../../../Auth/Auth";
 
 export default class ShareButton extends Component<{
 	entry: FeedEntry
@@ -18,15 +19,29 @@ export default class ShareButton extends Component<{
 	click = (e) => {
 		e.preventDefault();
 
-		// TODO
+		if (!this.isSelf()) {
+			// TODO
+		}
 	};
 
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		return <Col className={"text-center"}>
-			<div className={"d-inline-block shareButton" + (this.state.shared ? " active" : "")}
-				 onClick={(e) => this.click(e)}>
+			<div
+				className={"d-inline-block shareButton" + (this.state.shared ? " active" : this.isSelf() ? " inactive" : "")}
+				onClick={(e) => this.click(e)}>
 				<i className={"fas fa-retweet"}/><span className={"number"}>{this.props.entry.getShareCount()}</span>
 			</div>
 		</Col>;
+	}
+
+	private isSelf() {
+		let self: boolean = false;
+		const currentUser = Auth.getCurrentUser();
+
+		if (currentUser && this.props.entry.getUser().getId() === currentUser.getId()) {
+			self = true;
+		}
+
+		return self;
 	}
 }
