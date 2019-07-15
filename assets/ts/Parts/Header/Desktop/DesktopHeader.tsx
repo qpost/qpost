c97d5ed4eadba0ed50910b1601dd61606977dc6d
Zeryther <zeryther@gmail.com>
2019-07-15 18:40:25 +0200

    Updated desktop header navigation points

--- assets/ts/Parts/Header/Desktop/DesktopHeader.tsx
+++ assets/ts/Parts/Header/Desktop/DesktopHeader.tsx
@@ -3,12 +3,15 @@ import Logo from "../../../../img/navlogo.png";
 import {Layout, Menu} from "antd";
 import NightMode from "../../../NightMode/NightMode";
 import {Link} from "react-router-dom";
+import Auth from "../../../Auth/Auth";
 
 export default class DesktopHeader extends Component<{
 	mobile: boolean,
 	key: any
 }, any> {
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+		const currentUser = Auth.getCurrentUser();
+
 		return <Layout.Header
 			hasSider={true}
 			className={"mainNav"}
@@ -32,9 +35,29 @@ export default class DesktopHeader extends Component<{
 					lineHeight: "64px",
 					float: "right"
 				}}>
-				<Menu.Item key={0}>Nav 1</Menu.Item>
-				<Menu.Item key={1}>Nav 2</Menu.Item>
-				<Menu.Item key={3}>Nav 3</Menu.Item>
+				<Menu.Item key={0}>
+					<Link to={"/"} className={"clearUnderline"}>
+						home
+					</Link>
+				</Menu.Item>
+
+				<Menu.Item key={1}>
+					<Link to={"/" + currentUser.getUsername()} className={"clearUnderline"}>
+						my profile
+					</Link>
+				</Menu.Item>
+
+				<Menu.Item key={2}>
+					<Link to={"/notifications"} className={"clearUnderline"}>
+						notifications
+					</Link>
+				</Menu.Item>
+
+				<Menu.Item key={3}>
+					<Link to={"/messages"} className={"clearUnderline"}>
+						messages
+					</Link>
+				</Menu.Item>
 			</Menu>
 		</Layout.Header>;
 	}
