a298bf9a7ebe09a22c55e6c02ee4aff411a36717
Zeryther <zeryther@gmail.com>
2019-07-16 22:51:02 +0200

    Added button for DummyPostForm on mobile

--- assets/ts/Component/PostForm/DummyPostForm.tsx
+++ assets/ts/Component/PostForm/DummyPostForm.tsx
@@ -1,8 +1,14 @@
 import React, {Component} from "react";
 import Auth from "../../Auth/Auth";
-import {Card, Input} from "antd";
+import {Button, Card, Input} from "antd";
 
 export default class DummyPostForm extends Component<any, any> {
+	click = (e) => {
+		e.preventDefault();
+
+		// TODO: Open actual post form
+	};
+
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		const currentUser = Auth.getCurrentUser();
 
@@ -10,14 +16,21 @@ export default class DummyPostForm extends Component<any, any> {
 			return "";
 		}
 
-		return <Card className={"dummyPostForm desktopOnly mb-3 rounded"} size={"small"} onClick={(e) => {
-			e.preventDefault();
+		return [
+			<Card className={"dummyPostForm desktopOnly mb-3 rounded"} size={"small"} onClick={(e) => this.click(e)}>
+				<img src={currentUser.getAvatarURL()} className={"avatar"} alt={currentUser.getUsername()}/>
 
-			// TODO: Open actual post form
-		}}>
-			<img src={currentUser.getAvatarURL()} className={"avatar"} alt={currentUser.getUsername()}/>
-
-			<Input placeholder={"Post something for your followers!"} className={"fakeTextBox"}/>
-		</Card>;
+				<Input placeholder={"Post something for your followers!"} className={"fakeTextBox"}/>
+			</Card>,
+			<Button type={"primary"} size={"large"} shape={"circle"} className={"mobileOnly"}
+					onClick={(e) => this.click(e)} style={{
+				position: "fixed",
+				zIndex: 500,
+				bottom: 90,
+				right: 30
+			}}>
+				<i className="fas fa-pencil-alt"/>
+			</Button>
+		];
 	}
 }
