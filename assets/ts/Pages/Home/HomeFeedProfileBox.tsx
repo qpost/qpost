123bfe4468dc178e40b7631e5aa6467396bb79b6
Zeryther <zeryther@gmail.com>
2019-07-18 01:24:28 +0200

    Updated copyright notices

--- assets/ts/Pages/Home/HomeFeedProfileBox.tsx
+++ assets/ts/Pages/Home/HomeFeedProfileBox.tsx
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018-2019 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpo.st
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
 import React, {Component} from "react";
 import Auth from "../../Auth/Auth";
 import {Link} from "react-router-dom";
@@ -24,14 +43,14 @@ export default class HomeFeedProfileBox extends Component<any, any> {
 					<div className="ml-2 float-left mt-1">
 						<Link to={"/" + currentUser.getUsername()} className="clearUnderline float-left">
 							<div className="font-weight-bold"
-								style={{
-									maxWidth: "70px",
-									overflow: "hidden",
-									textOverflow: "ellipsis",
-									whiteSpace: "nowrap",
-									wordWrap: "normal",
-									marginTop: "-5px"
-								}}>
+								 style={{
+									 maxWidth: "70px",
+									 overflow: "hidden",
+									 textOverflow: "ellipsis",
+									 whiteSpace: "nowrap",
+									 wordWrap: "normal",
+									 marginTop: "-5px"
+								 }}>
 								{currentUser.getDisplayName()}<VerifiedBadge target={currentUser}/>
 							</div>
 
