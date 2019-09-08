aaa21bcbafcd7db7e0be3060677ce1a6f2137c1d
Zeryther <zeryther@gmail.com>
2019-09-08 23:09:33 +0200

    Added ReactController

---
+++ src/Controller/ReactController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright (C) 2018-2019 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpo.st
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+namespace qpost\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReactController extends AbstractController {
+	/**
+	 * @Route("/{reactRouting}", defaults={"reactRouting": null})
+	 */
+	public function index() {
+		return $this->render("react.html.twig");
+	}
+}
