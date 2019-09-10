d81ff558b5b340ed84812acfaa9887140caad7d9
Zeryther <zeryther@gmail.com>
2019-09-10 19:40:35 +0200

    Added StatusController

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (C) 2018-2019 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpo.st
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+namespace qpost\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use qpost\Entity\FeedEntry;
+use qpost\Twig\Twig;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatusController extends AbstractController {
+	/**
+	 * @Route("/status/{id}")
+	 *
+	 * @param int $id
+	 * @param EntityManagerInterface $entityManager
+	 * @return Response
+	 */
+	public function index(int $id, EntityManagerInterface $entityManager) {
+		/**
+		 * @var FeedEntry $feedEntry
+		 */
+		$feedEntry = $entityManager->getRepository(FeedEntry::class)->findOneBy([
+			"id" => $id
+		]);
+
+		if (!is_null($feedEntry)) {
+			return $this->render("react.html.twig", Twig::param());
+		}
+
+		throw $this->createNotFoundException("Invalid status ID.");
+	}
+}
