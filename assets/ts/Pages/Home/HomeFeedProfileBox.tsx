dc3a117d8528150bcf4a482c6798e15a6d778f0f
Zeryther <zeryther@gmail.com>
2019-09-15 18:22:15 +0200

    Updated HomeFeedProfileBox style to show more info

--- assets/ts/Pages/Home/HomeFeedProfileBox.tsx
+++ assets/ts/Pages/Home/HomeFeedProfileBox.tsx
@@ -29,88 +29,103 @@ export default class HomeFeedProfileBox extends Component<any, any> {
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		const currentUser = Auth.getCurrentUser();
 
-		if(!currentUser){
+		if (!currentUser) {
 			Auth.logout();
 			return null;
 		}
 
 		return <Card className="homeFeedProfileBox mb-3" size={"small"}>
-			<div className="px-2 py-2">
-				<div className="d-block" style={{height: "50px"}}>
-					<Link to={"/" + currentUser.getUsername()} className="clearUnderline float-left">
-						<img src={currentUser.getAvatarURL()} className="rounded" width="48" height="48" alt={currentUser.getUsername()}/>
-					</Link>
+			<div className="d-block" style={{height: "50px"}}>
+				<Link to={"/" + currentUser.getUsername()} className="clearUnderline float-left">
+					<img src={currentUser.getAvatarURL()} className="rounded" width="48" height="48"
+						 alt={currentUser.getUsername()}/>
+				</Link>
 
-					<div className="ml-2 float-left mt-1">
-						<Link to={"/" + currentUser.getUsername()} className="clearUnderline float-left">
-							<div className="font-weight-bold"
-								 style={{
-									 maxWidth: "70px",
-									 overflow: "hidden",
-									 textOverflow: "ellipsis",
-									 whiteSpace: "nowrap",
-									 wordWrap: "normal",
-									 marginTop: "-5px"
-								 }}>
-								{currentUser.getDisplayName()}<VerifiedBadge target={currentUser}/>
-							</div>
+				<div className="ml-2 float-left mt-1" style={{
+					width: "calc(100% - 150px)"
+				}}>
+					<div style={{
+						maxWidth: "calc(100% - 10px)",
+						overflow: "hidden",
+						textOverflow: "ellipsis",
+						whiteSpace: "nowrap",
+						wordWrap: "normal"
+					}}>
+						<Link to={"/" + currentUser.getUsername()} className="clearUnderline">
+								<span className="font-weight-bold"
+									  style={{
+										  marginTop: "-5px",
+										  maxWidth: "100%",
+										  display: "inline-block",
+										  overflow: "hidden",
+										  textOverflow: "ellipsis",
+										  whiteSpace: "nowrap",
+										  wordWrap: "normal"
+									  }}>
+									{currentUser.getDisplayName()}<VerifiedBadge target={currentUser}/>
+								</span>
+						</Link>
 
-							<div className="text-muted small"
-								 style={{
-									 maxWidth: "70px",
-									 overflow: "hidden",
-									 textOverflow: "ellipsis",
-									 whiteSpace: "nowrap",
-									 wordWrap: "normal",
-									 marginTop: "-5px"
-								 }}>
-								@{currentUser.getUsername()}
-							</div>
+						<br/>
+
+						<Link to={"/" + currentUser.getUsername()} className="clearUnderline">
+								<span className="text-muted small"
+									  style={{
+										  marginTop: "-5px",
+										  maxWidth: "100%",
+										  display: "inline-block",
+										  overflow: "hidden",
+										  textOverflow: "ellipsis",
+										  whiteSpace: "nowrap",
+										  wordWrap: "normal"
+									  }}>
+									@{currentUser.getUsername()}
+								</span>
 						</Link>
 					</div>
-
-					<FollowButton target={currentUser} className={"float-right mt-2"} size={"small"}/>
 				</div>
 
-				<hr className="mb-2 mt-3"/>
+				<FollowButton target={currentUser} className={"float-right mt-2"} size={"small"}/>
+			</div>
 
-				<div>
-					<Link to={"/" + currentUser.getUsername()} className="clearUnderline mb-1">
-						<div style={{height: "24px"}}>
-							<div className="text-muted text-uppercase small float-left pt-1">
-								Posts
-							</div>
+			<hr className="mb-2 mt-3"/>
 
-							<div className="font-weight-bold text-uppercase float-right">
-								{formatNumberShort(currentUser.getPostCount())}
-							</div>
+			<div>
+				<Link to={"/" + currentUser.getUsername()} className="clearUnderline mb-1">
+					<div style={{height: "24px"}}>
+						<div className="text-muted text-uppercase small float-left pt-1">
+							Posts
 						</div>
-					</Link>
 
-					<Link to={"/" + currentUser.getUsername() + "/following"} className="clearUnderline mb-1">
-						<div style={{height: "24px"}}>
-							<div className="text-muted text-uppercase small float-left pt-1">
-								Following
-							</div>
-
-							<div className="font-weight-bold text-uppercase float-right">
-								{formatNumberShort(currentUser.getFollowingCount())}
-							</div>
+						<div className="font-weight-bold text-uppercase float-right">
+							{formatNumberShort(currentUser.getPostCount())}
 						</div>
-					</Link>
+					</div>
+				</Link>
 
-					<Link to={"/" + currentUser.getUsername() + "/followers"} className="clearUnderline mb-1">
-						<div style={{height: "24px"}}>
-							<div className="text-muted text-uppercase small float-left pt-1">
-								Followers
-							</div>
-
-							<div className="font-weight-bold text-uppercase float-right">
-								{formatNumberShort(currentUser.getFollowerCount())}
-							</div>
+				<Link to={"/" + currentUser.getUsername() + "/following"} className="clearUnderline mb-1">
+					<div style={{height: "24px"}}>
+						<div className="text-muted text-uppercase small float-left pt-1">
+							Following
 						</div>
-					</Link>
-				</div>
+
+						<div className="font-weight-bold text-uppercase float-right">
+							{formatNumberShort(currentUser.getFollowingCount())}
+						</div>
+					</div>
+				</Link>
+
+				<Link to={"/" + currentUser.getUsername() + "/followers"} className="clearUnderline mb-1">
+					<div style={{height: "24px"}}>
+						<div className="text-muted text-uppercase small float-left pt-1">
+							Followers
+						</div>
+
+						<div className="font-weight-bold text-uppercase float-right">
+							{formatNumberShort(currentUser.getFollowerCount())}
+						</div>
+					</div>
+				</Link>
 			</div>
 		</Card>
 	}
