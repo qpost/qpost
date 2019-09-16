1b80b5867d67c9eace75fad6e983a0841f934fd5
Zeryther <zeryther@gmail.com>
2019-09-16 02:21:59 +0200

    Use redirect route in links in SuggestedUsers

--- assets/ts/Component/SuggestedUsers.tsx
+++ assets/ts/Component/SuggestedUsers.tsx
@@ -63,14 +63,13 @@ export default class SuggestedUsers extends Component<any, { loading: boolean, r
 				<div className="tab-content" id="users-tablist-content">
 					{this.state.results.map((suggestedUser, i) => {
 						return <div className="my-1" style={{height: "70px"}} key={i}>
-							<Link to={"/" + suggestedUser.getUsername()} className="clearUnderline float-left">
+							<Link to={"/profile/" + suggestedUser.getUsername()} className="clearUnderline float-left">
 								<img src={suggestedUser.getAvatarURL()} width="64" height="64" className="rounded"
 									 alt={suggestedUser.getUsername()}/>
 							</Link>
 
 							<div className="ml-2 float-left">
-								<Link to={"/" + suggestedUser.getUsername()} className="clearUnderline"
-									  data-user-id="{{ suggestedUser.id }}">
+								<Link to={"/profile/" + suggestedUser.getUsername()} className="clearUnderline">
 									<div className="float-left mt-1"
 										 style={{
 											 maxWidth: "220px",
