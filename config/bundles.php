9aa468d9b23cccec10161c116611d08a503f543d
Zeryther <zeryther@gmail.com>
2019-10-22 22:28:53 +0200

    Added Sentry support to backend

--- config/bundles.php
+++ config/bundles.php
@@ -32,4 +32,5 @@ return [
 	Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 	Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 	Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+	Sentry\SentryBundle\SentryBundle::class => ['all' => true]
 ];
