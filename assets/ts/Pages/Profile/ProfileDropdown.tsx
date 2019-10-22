2fa6beecea82c080811309c2b7d8832bfa7703a8
Zeryther <zeryther@gmail.com>
2019-10-23 00:09:30 +0200

    Fixed block option showing up in ProfileDropdown even when not logged in

--- assets/ts/Pages/Profile/ProfileDropdown.tsx
+++ assets/ts/Pages/Profile/ProfileDropdown.tsx
@@ -47,7 +47,7 @@ export default class ProfileDropdown extends Component<{
 						break;
 				}
 			}}>
-				{(!Auth.isLoggedIn() || this.props.user.getId() !== Auth.getCurrentUser().getId()) && !this.props.user.isBlocked() ?
+				{Auth.isLoggedIn() && this.props.user.getId() !== Auth.getCurrentUser().getId() && !this.props.user.isBlocked() ?
 					<Menu.Item key={"1"}>
 					<i className="fas fa-ban iconMargin-10"/>Block {"@" + this.props.user.getUsername()}
 				</Menu.Item> : ""}
