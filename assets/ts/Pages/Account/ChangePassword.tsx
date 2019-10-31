0b17e086316dad92cdff10ee0171d726f5ad396b
Zeryther <zeryther@gmail.com>
2019-10-31 14:51:29 +0100

    Added change password page

--- assets/ts/Pages/Account/ChangePassword.tsx
+++ assets/ts/Pages/Account/ChangePassword.tsx
@@ -19,17 +19,121 @@
 
 import React, {Component} from "react";
 import AccountBase from "./AccountBase";
+import {Button, Card, Col, Input, message, Row} from "antd";
+import ReturnHeader from "../../Component/ReturnHeader";
+import API from "../../API/API";
+import Auth from "../../Auth/Auth";
+import BaseObject from "../../Serialization/BaseObject";
+import User from "../../Entity/Account/User";
 
-export default class ChangePassword extends Component<any, any> {
+export default class ChangePassword extends Component<any, {
+	oldPassword: string,
+	newPassword: string,
+	newPassword2: string,
+	loading: boolean
+}> {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			oldPassword: "",
+			newPassword: "",
+			newPassword2: "",
+			loading: false
+		};
 	}
 
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+		const labelCol = {xs: {span: 6, offset: 0}, md: {span: 3, offset: 0}};
+		const wrapperCol = {xs: {span: 18, offset: 0}, md: {span: 21, offset: 0}};
+		const labelStyle = {lineHeight: "25px"};
+
 		return <AccountBase activeKey={"HOME"}>
-			password
+			<ReturnHeader className={"mb-3"}/>
+
+			<Card size={"small"} title={"Change password"}>
+				<Row className={"mb-3"}>
+					<Col {...labelCol} style={labelStyle}>
+						Current password
+					</Col>
+
+					<Col {...wrapperCol}>
+						<Input.Password value={this.state.oldPassword} onChange={(e) => {
+							const value = e.target.value;
+
+							this.setState({
+								oldPassword: value
+							});
+						}} visibilityToggle={false}/>
+					</Col>
+				</Row>
+
+				<Row className={"mb-3"}>
+					<Col {...labelCol} style={labelStyle}>
+						New password
+					</Col>
+
+					<Col {...wrapperCol}>
+						<Input.Password value={this.state.newPassword} onChange={(e) => {
+							const value = e.target.value;
+
+							this.setState({
+								newPassword: value
+							});
+						}} visibilityToggle={false}/>
+					</Col>
+				</Row>
+
+				<Row className={"mb-3"}>
+					<Col {...labelCol} style={labelStyle}>
+						Repeat new password
+					</Col>
+
+					<Col {...wrapperCol}>
+						<Input.Password value={this.state.newPassword2} onChange={(e) => {
+							const value = e.target.value;
+
+							this.setState({
+								newPassword2: value
+							});
+						}} visibilityToggle={false}/>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col {...labelCol}>
+						&nbsp;
+					</Col>
+
+					<Col {...wrapperCol}>
+						<Button type={"primary"} loading={this.state.loading} onClick={(e) => {
+							if (!this.state.loading) {
+								if (this.state.newPassword === this.state.newPassword2) {
+									this.setState({loading: true});
+
+									API.handleRequest("/password", "POST", this.state, data => {
+										this.setState({
+											loading: false,
+											oldPassword: "",
+											newPassword: "",
+											newPassword2: ""
+										});
+										Auth.setCurrentUser(BaseObject.convertObject(User, data.result));
+										message.success("Your password has been changed.");
+									}, error => {
+										this.setState({loading: false});
+										message.error(error);
+									});
+								} else {
+									message.error("The new passwords don't match.");
+								}
+							}
+						}}>
+							Save changes
+						</Button>
+					</Col>
+				</Row>
+			</Card>
 		</AccountBase>;
 	}
 }
