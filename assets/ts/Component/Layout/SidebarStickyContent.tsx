e9755dc63657f57a9e7f85e1cad7fd99c3072d0e
Zeryther <zeryther@gmail.com>
2019-11-02 20:08:35 +0100

    Updatd npm dependencies

--- assets/ts/Component/Layout/SidebarStickyContent.tsx
+++ assets/ts/Component/Layout/SidebarStickyContent.tsx
@@ -45,7 +45,9 @@ export default class SidebarStickyContent extends Component<any, {
 
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		return [this.state.mobile === false ? <Affix offsetTop={70}>
-			{this.props.children}
+			<div>
+				{this.props.children}
+			</div>
 		</Affix> : this.props.children, <WindowSizeListener onResize={windowSize => {
 			this.setIsMobileMenu(windowSize.windowWidth);
 		}}/>];
