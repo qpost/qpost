d2dd8eb1108d74c1512503bb91cf15c9992a7c85
Zeryther <zeryther@gmail.com>
2019-11-02 15:27:52 +0100

    Added stillGIFURL function

--- assets/ts/Util/Format.ts
+++ assets/ts/Util/Format.ts
@@ -46,6 +46,12 @@ export function cacheImage(url: string): string {
 	return "https://images.weserv.nl/?url=" + encodeURI(url);
 }
 
+export function stillGIFURL(url: string): string {
+	url = url.toLowerCase();
+
+	return url.endsWith(".gif") ? cacheImage(url) : url;
+}
+
 export function convertUserAgentToIconClass(userAgent: UAParser): string {
 	const browserName: string | undefined = userAgent.getBrowser().name;
 	if (browserName !== undefined) {
