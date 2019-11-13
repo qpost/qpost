9b490667bfe5ac9ad7b146961f515aa068965899
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2019-11-13 19:49:59 +0100

    Consider hashtags in Linkifier

--- assets/ts/Component/Linkifier.tsx
+++ assets/ts/Component/Linkifier.tsx
@@ -56,6 +56,32 @@ export default class Linkifier extends Component<any, any> {
 				}
 			});
 
+			linkify.add('#', {
+				validate: function (text, pos, self) {
+					var tail = text.slice(pos);
+
+					if (!self.re.twitter) {
+						self.re.twitter = new RegExp(
+							'^([a-zA-Z0-9_]){1,64}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+						);
+					}
+
+					if (self.re.twitter.test(tail)) {
+						if (pos >= 2 && tail[pos - 2] === '#') {
+							return false;
+						}
+
+						return tail.match(self.re.twitter)[0].length;
+					}
+
+					return 0;
+				},
+
+				normalize: function (match) {
+					match.url = '/r/search/' + encodeURI(match.url);
+				}
+			});
+
 			let match = linkify.match(text);
 
 			if (match) {
