4aac26e1952af43f969c4751c47837d0e3b8966e
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2019-11-14 20:26:02 +0100

    Added service worker

---
+++ public/sw.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2018-2019 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpo.st
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+const CACHE_NAME = "qpost-sw-precache";
+const PRECACHE_FILES = [];
+const LOG_PREFIX = "[QPOST-SW] ";
+const OFFLINE_PAGE = "/offline.html";
+
+self.addEventListener("install", (event) => {
+	console.log(LOG_PREFIX + "Installing");
+	console.log(LOG_PREFIX + "Skip waiting on install");
+	self.skipWaiting();
+
+	// Add assets to cache
+	event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+		console.log(LOG_PREFIX + "Caching pages during install");
+
+		return cache.addAll(PRECACHE_FILES);
+	}));
+});
+
+self.addEventListener("activate", (event) => {
+	console.log(LOG_PREFIX + "Claiming clients for current page");
+
+	event.waitUntil(self.clients.claim());
+});
+
+
+self.addEventListener("fetch", (event) => {
+	// Serve from cache if a fetch fails
+	const request = event.request;
+
+	// only cache HTTP GET requests
+	if (request.method !== "GET") return;
+
+	event.respondWith(loadFromCache(request).then(
+		(response => {
+			// Response was cached
+
+			// Update cache
+			event.waitUntil(fetch(request).then((response) => {
+				return updateCache(request, response);
+			}));
+
+			return response;
+		}),
+
+		(() => {
+			// Response was not cached
+
+			return fetch(request).then(response => {
+				event.waitUntil(updateCache(request, response.clone()));
+
+				return response;
+			}).catch(error => {
+				console.error(LOG_PREFIX + "Network request failed for uncached resource.", error);
+				return caches.open(CACHE_NAME).then(cache => {
+					return cache.match(OFFLINE_PAGE)
+				});
+			});
+		})
+	).catch(error => {
+		if (request.destination !== "document" || request.mode !== "navigate") return;
+
+		console.error(LOG_PREFIX + "Network request failed. Serving offline page", error);
+		return caches.open(CACHE_NAME).then(cache => {
+			return cache.match(OFFLINE_PAGE)
+		});
+	}));
+});
+
+self.addEventListener("refreshOffline", () => {
+	const offlinePageRequest = new Request(OFFLINE_PAGE);
+
+	return fetch(OFFLINE_PAGE).then((response) => {
+		return updateCache(offlinePageRequest, response);
+	});
+});
+
+loadFromCache = request => {
+	return caches.open(CACHE_NAME).then(cache => {
+		return cache.match(request).then(matching => {
+			if (!matching || matching.status === 404) {
+				return Promise.reject("No match was found.");
+			}
+
+			return matching;
+		});
+	});
+};
+
+updateCache = (request, response) => {
+	return caches.open(CACHE_NAME).then(cache => {
+		return cache.put(request, response);
+	});
+};
