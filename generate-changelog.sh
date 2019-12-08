0b1ba66b487c1c33a0cc9313756af6e37159e014
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2019-12-08 16:46:08 +0100

    Formatted script

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git log --date=format:'%a %b %d, %Y' --all --pretty=format:"* **%ad** %d %s (%aN)" --no-merges --invert-grep --grep="Updated version number" > CHANGELOG.md
+git log --date=format:'%a %b %d, %Y' --all --pretty=format:"* **%ad** %d %s (%aN)" --no-merges --invert-grep --grep="Updated version number" >CHANGELOG.md
