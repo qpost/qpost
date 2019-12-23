e81b78f0dd5b75dc40638e139ccf5604e47a2545
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2019-12-23 22:31:04 +0100

    Use ProfileRedirect in FeedEntryListItem

--- assets/ts/Component/FeedEntry/FeedEntryListItem.tsx
+++ assets/ts/Component/FeedEntry/FeedEntryListItem.tsx
@@ -110,14 +110,14 @@ export default class FeedEntryListItem extends Component<{
 								flexWrap: "wrap"
 							}}>
 								<div className={"float-left"}>
-									<Link to={"/" + user.getUsername()} className={"clearUnderline float-left"}>
+									<Link to={"/profile/" + user.getUsername()} className={"clearUnderline float-left"}>
 										<img className={"rounded mx-1 my-1"} src={entry.getUser().getAvatarURL()}
 											 width={40}
 											 height={40} alt={entry.getUser().getUsername()}/>
 									</Link>
 
 									<p className={"float-left ml-1 mb-0"}>
-										<Link to={"/" + user.getUsername()} className={"clearUnderline"}>
+										<Link to={"/profile/" + user.getUsername()} className={"clearUnderline"}>
 										<span className={"font-weight-bold convertEmoji mr-2"}>
 											{user.getDisplayName()}<VerifiedBadge target={user}/>
 										</span>
