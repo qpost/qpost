8a8b46363529ff3168afc4170e8a98634f42a092
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-01-26 20:08:46 +0100

    Fixed migration 20200120200708

--- src/Migrations/Version20200120200708.php
+++ src/Migrations/Version20200120200708.php
@@ -37,9 +37,6 @@ final class Version20200120200708 extends AbstractMigration {
 		// this up() migration is auto-generated, please modify it to your needs
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-		$this->addSql('ALTER TABLE poll_vote DROP FOREIGN KEY FK_ED568EBE3C947C0F');
-		$this->addSql('DROP TABLE poll');
-		$this->addSql('DROP TABLE poll_vote');
 		$this->addSql('ALTER TABLE user_appearance_settings ADD night_mode TINYINT(1) NOT NULL, ADD autoplay_gifs TINYINT(1) NOT NULL, ADD show_trends TINYINT(1) NOT NULL, ADD show_suggested_users TINYINT(1) NOT NULL, ADD show_birthdays TINYINT(1) NOT NULL, ADD show_mature_warning TINYINT(1) NOT NULL');
 	}
 
@@ -47,11 +44,6 @@ final class Version20200120200708 extends AbstractMigration {
 		// this down() migration is auto-generated, please modify it to your needs
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-		$this->addSql('CREATE TABLE poll (id INT AUTO_INCREMENT NOT NULL, feed_entry_id INT NOT NULL, options LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:array)\', expiry DATETIME NOT NULL, time DATETIME NOT NULL, winner INT DEFAULT NULL, UNIQUE INDEX UNIQ_84BCFA459AE3DEE5 (feed_entry_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-		$this->addSql('CREATE TABLE poll_vote (id INT AUTO_INCREMENT NOT NULL, poll_id INT NOT NULL, user_id INT NOT NULL, selected_option INT NOT NULL, time DATETIME NOT NULL, UNIQUE INDEX UNIQ_ED568EBE3C947C0FA76ED395 (poll_id, user_id), INDEX IDX_ED568EBE3C947C0F (poll_id), INDEX IDX_ED568EBEA62CADF7 (selected_option), INDEX IDX_ED568EBEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-		$this->addSql('ALTER TABLE poll ADD CONSTRAINT FK_84BCFA459AE3DEE5 FOREIGN KEY (feed_entry_id) REFERENCES feed_entry (id)');
-		$this->addSql('ALTER TABLE poll_vote ADD CONSTRAINT FK_ED568EBE3C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id)');
-		$this->addSql('ALTER TABLE poll_vote ADD CONSTRAINT FK_ED568EBEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
 		$this->addSql('ALTER TABLE user_appearance_settings DROP night_mode, DROP autoplay_gifs, DROP show_trends, DROP show_suggested_users, DROP show_birthdays, DROP show_mature_warning');
 	}
 }
