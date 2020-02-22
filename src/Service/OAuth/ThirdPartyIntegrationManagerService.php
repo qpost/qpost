43241f1c226e1255e92339bbf17f1553561ed5e2
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-02-22 03:55:51 +0100

    Added ThirdPartyIntegrationManagerService class to easily access all ThirdPartyIntegration classes

---
+++ src/Service/OAuth/ThirdPartyIntegrationManagerService.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (C) 2018-2020 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpo.st
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+namespace qpost\Service\OAuth;
+
+use function strtoupper;
+
+class ThirdPartyIntegrationManagerService {
+	/**
+	 * @var ThirdPartyIntegration[] $integrationServices
+	 */
+	private $integrationServices;
+
+	/**
+	 * ThirdPartyIntegrationManagerService constructor.
+	 * @param DiscordIntegration $discordIntegration
+	 * @param TwitchIntegration $twitchIntegration
+	 * @param TwitterIntegration $twitterIntegration
+	 */
+	public function __construct(
+		DiscordIntegration $discordIntegration,
+		TwitchIntegration $twitchIntegration,
+		TwitterIntegration $twitterIntegration
+	) {
+		$this->integrationServices = [
+			$discordIntegration,
+			$twitchIntegration,
+			$twitterIntegration
+		];
+	}
+
+	/**
+	 * @return ThirdPartyIntegration[]
+	 */
+	public function getIntegrationServices(): array {
+		return $this->integrationServices;
+	}
+
+	/**
+	 * Get an integration service from it's identifier.
+	 * @param string $identifier
+	 * @return ThirdPartyIntegration|null
+	 */
+	public function getIntegrationService(string $identifier): ?ThirdPartyIntegration {
+		foreach ($this->integrationServices as $service) {
+			$serviceIdentifier = $service->getServiceIdentifier();
+
+			if (!is_null($serviceIdentifier) && strtoupper($serviceIdentifier) === strtoupper($identifier)) {
+				return $service;
+			}
+		}
+
+		return null;
+	}
+}
