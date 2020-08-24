ea5faee980ae23db480388c7b486fd3de90955f6
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-08-25 00:47:08 +0200

    Reload page if not logged in and rendering React

--- assets/ts/Pages/Home/Home.tsx
+++ assets/ts/Pages/Home/Home.tsx
@@ -20,6 +20,10 @@
 import React, {Component} from "react";
 
 export default class Home extends Component<any, any> {
+	componentDidMount() {
+		window.location.href = "/";
+	}
+
 	render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		// TODO
 		return <div>Please log in.</div>;
